1f0c25399c29dd966689aee52547877359c0eb55
mathur619 <prakharmathur619@gmail.com>
2022-06-28 00:25:29 +0530

    react toastify added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
 function App() {
   return (
     <div className="App">
       <AppRoutes />
+      <ToastContainer
+        newestOnTop
+        position="top-center"
+        autoClose={2000}
+        transition={Slide}
+        toastClassName="App-toast"
+      />
     </div>
   );
 }
